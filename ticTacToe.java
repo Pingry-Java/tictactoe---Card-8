bd27746aa51c59c386c2c363dd862ee2c671ebf7
Dillon Shu <shudillon@gmail.com>
2016-12-01 08:35:49 -0500

    Initial Sketch w/ Javadoc

---
+++ ticTacToe.java
@@ -0,0 +1,53 @@
+/*
+1.Write API Specification (Javadoc) for the program
+2.Write all the methods
+3.Write the main program
+*/
+public class ticTacToe
+{
+	/**
+	*Main function where the game is played. The coding for the game should be here.
+	*/
+	public static void main ()
+	{
+		
+	}
+	/**
+	*Creates a tic tac toe board to use
+	*@return a String[][] a 2D string array that is numbered from 0-8
+	*/
+	public static String[][] buildBoard()
+	{
+		
+	}
+	/**
+	*Prints out the tic tac toe board in a nice format with lines
+	*@param s Array which needs to be formatted and printed
+	*/
+	public static void printBoard(String[][] s)
+	{
+		
+	}
+	/**
+	*Gets the user's move and updates the String array with the player's symbol
+	*@param position The spot where the user wants to move
+	*@param userSymbol The symbol a user is using as their "piece"
+	*@param board The board before the user's move.
+	*@return Updated board with user's piece on it.
+	*/
+	public static String[][] move(int position, String userSymbol, String[][] board)
+	{
+		
+	}
+	/**
+	*Checks the current board if there has been a winner (three in a row)
+	*
+	*@param s Current board which needs to be checked for a win
+	*@return Whether or not there is three in a row (true for winner, false for no winner yet)
+	*/
+	public static boolean winConditions(String[][] s)
+	{
+		
+	}
+	
+}
