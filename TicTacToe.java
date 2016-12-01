6558ccd506f6939c30b3b4de28bb117e5a73ab1b
Miro Bergam <mbergam2019@pingry.org>
2016-12-01 08:34:03 -0500

    Started API

---
+++ TicTacToe.java
@@ -0,0 +1,58 @@
+public class TicTacToe
+{
+	
+	public static void main(String[] args)
+	{
+	}
+	
+	
+	/**
+	* Initializes an int array that is our board
+	* @param size The size of the length/width of the board
+	* @return Returns the initialized array
+	*/
+	public static String[][] init(int size)
+	{
+		
+	}
+	
+	/**
+	* Prints the tictactoe board
+	* @param array The array that we will print into a board
+	*/
+	public static void printGrid(String[][] array)
+	{
+	
+	}
+	
+	/**
+	* Calls upon checkwinner for all directions
+	* @param array The current board
+	* @return Returns if there is a winner
+	*/
+	public static boolean checkWinner(String[][] array)
+	
+	/**
+	
+	*/
+	public static boolean checkDiag()
+	
+	/**
+	
+	*/
+	public static boolean checkReverseDiag()
+	
+	/**
+	
+	*/
+	public static boolean checkVert()
+	
+	/**
+	
+	*/
+	public static boolean checkHoriz()
+	
+	
+	
+	
+}
